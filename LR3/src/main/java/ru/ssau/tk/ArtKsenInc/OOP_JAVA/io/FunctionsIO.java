1c40d031a92cb4c96378af8a3d94d7520037964a
Kseteny <1kseteni1@gmail.com>
2024-10-22 23:36:07 +0400

    Y
    Signed-off-by: ResArt2005 <resart2005@gmail.com>

--- LR3/src/main/java/ru/ssau/tk/ArtKsenInc/OOP_JAVA/io/FunctionsIO.java
+++ LR3/src/main/java/ru/ssau/tk/ArtKsenInc/OOP_JAVA/io/FunctionsIO.java
@@ -12,19 +12,16 @@ final public class FunctionsIO {
     public static void writeTabulatedFunction(BufferedWriter writer, TabulatedFunction function){
          // Создаем PrintWriter, оборачивая переданный BufferedWriter
         PrintWriter printWriter = new PrintWriter(writer);
-
         // Получаем количество точек в функции
         int count = function.getCount();
         // Записываем количество точек в первой строке
         printWriter.println(count);
-
         // Перебираем точки функции и записываем каждую из них
         for (Point point : function) {  // Предполагается, что TabulatedFunction реализует Iterable<Point>
             double x = point.x; // Получаем x
             double y = point.y; // Получаем y
             printWriter.printf("%f %f%n", x, y); // Записываем в формате "x y"
         }
-
         // Пробрасываем данные из буфера
         printWriter.flush();
     }
--- LR3/src/main/java/ru/ssau/tk/ArtKsenInc/OOP_JAVA/io/TabulatedFunctionFileWriter.java
+++ LR3/src/main/java/ru/ssau/tk/ArtKsenInc/OOP_JAVA/io/TabulatedFunctionFileWriter.java
@@ -14,7 +14,6 @@ public class TabulatedFunctionFileWriter {
         // Шаг 1: Определяем пути к файлам
         String arrayFunctionFilePath = "output/array_function.txt";
         String linkedListFunctionFilePath = "output/linked_list_function.txt";
-
         // Шаг 2: Используем try-with-resources для создания потоков
         try (
                 BufferedWriter arrayFunctionWriter = new BufferedWriter(new FileWriter(arrayFunctionFilePath));
@@ -23,7 +22,6 @@ public class TabulatedFunctionFileWriter {
             // Шаг 3: Создаем табулированные функции
             TabulatedFunction arrayFunction = new ArrayTabulatedFunction(new SqrFunction(), 0, 10, 5);
             TabulatedFunction linkedListFunction = new LinkedListTabulatedFunction(new double[]{1,2,3}, new double[]{1, 4, 9});
-
             // Шаг 4: Записываем функции в соответствующие файлы
             FunctionsIO.writeTabulatedFunction(arrayFunctionWriter, arrayFunction);
             FunctionsIO.writeTabulatedFunction(linkedListFunctionWriter, linkedListFunction);
