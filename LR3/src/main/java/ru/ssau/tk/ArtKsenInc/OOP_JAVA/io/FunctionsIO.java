9d1bd76773bc81b6a008eb820305ac1defa4b3f2
ResArt2005 <resart2005@gmail.com>
2024-10-23 21:31:12 +0400

    deserialize
    Signed-off-by: ResArt2005 <resart2005@gmail.com>

--- LR3/src/main/java/ru/ssau/tk/ArtKsenInc/OOP_JAVA/io/FunctionsIO.java
+++ LR3/src/main/java/ru/ssau/tk/ArtKsenInc/OOP_JAVA/io/FunctionsIO.java
@@ -79,4 +79,8 @@ final public class FunctionsIO {
         objectOutputStream.writeObject(function);
         objectOutputStream.flush();
     }
+    public static TabulatedFunction deserialize(BufferedInputStream stream) throws IOException, ClassNotFoundException {
+        ObjectInputStream objectInputStream = new ObjectInputStream(stream);
+        return (TabulatedFunction) objectInputStream.readObject();
+    }
 }
