217a2abd488ce0977de825b22e274a1b9ebed775
Kseteny <1kseteni1@gmail.com>
2024-11-02 14:38:38 +0400

    MultiplyingTaskExecutor*

--- LR4/src/main/java/ru/ssau/tk/ArtKsenInc/OOP_JAVA/concurrent/MultiplyingTask.java
+++ LR4/src/main/java/ru/ssau/tk/ArtKsenInc/OOP_JAVA/concurrent/MultiplyingTask.java
@@ -4,17 +4,25 @@ import ru.ssau.tk.ArtKsenInc.OOP_JAVA.functions.TabulatedFunction;
 
 public class MultiplyingTask implements Runnable {
     private final TabulatedFunction tabulatedFunction;
+    private volatile boolean completed = false; // Статус выполнения задачи
+
     public MultiplyingTask(TabulatedFunction tabulatedFunction) {
         this.tabulatedFunction = tabulatedFunction;
     }
+
     @Override
     public void run() {
-        synchronized (tabulatedFunction) { // Синхронизация доступа к табулированной функции
+        synchronized (tabulatedFunction) {
             for (int i = 0; i < tabulatedFunction.getCount(); i++) {
                 double newValue = tabulatedFunction.getY(i) * 2;
                 tabulatedFunction.setY(i, newValue);
             }
+            completed = true; // Устанавливаем статус выполнения
             System.out.println(Thread.currentThread().getName() + " completed task");
         }
+    }
+
+    public boolean isCompleted() {
+        return completed;
     }
 }
--- LR4/src/main/java/ru/ssau/tk/ArtKsenInc/OOP_JAVA/concurrent/MultiplyingTaskExecutor.java
+++ LR4/src/main/java/ru/ssau/tk/ArtKsenInc/OOP_JAVA/concurrent/MultiplyingTaskExecutor.java
@@ -4,21 +4,29 @@ import ru.ssau.tk.ArtKsenInc.OOP_JAVA.functions.LinkedListTabulatedFunction;
 import ru.ssau.tk.ArtKsenInc.OOP_JAVA.functions.UnitFunction;
 
 import java.util.ArrayList;
+import java.util.List;
+import java.util.Iterator;
 
 public class MultiplyingTaskExecutor {
     public static void main(String[] args) {
         LinkedListTabulatedFunction tabulatedFunction = new LinkedListTabulatedFunction(new UnitFunction(), 1, 1000, 1000);
-        ArrayList<Thread> threadList = new ArrayList<>();
+        List<MultiplyingTask> tasks = new ArrayList<>();
+        List<Thread> threads = new ArrayList<>();
         for (int i = 0; i < 10; i++) {
             MultiplyingTask task = new MultiplyingTask(tabulatedFunction);
             Thread thread = new Thread(task);
-            threadList.add(thread);
+            tasks.add(task);
+            threads.add(thread);
             thread.start();
         }
-        try {
-            Thread.sleep(2000);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
+        while (!tasks.isEmpty()) {
+            Iterator<MultiplyingTask> iterator = tasks.iterator();
+            while (iterator.hasNext()) {
+                MultiplyingTask task = iterator.next();
+                if (task.isCompleted()) {
+                    iterator.remove();
+                }
+            }
         }
         System.out.println(tabulatedFunction);
     }
