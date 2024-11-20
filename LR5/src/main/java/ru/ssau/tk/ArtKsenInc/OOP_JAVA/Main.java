4ef6f06c1262816afb750f4a36f9318aed7b08d2
ResArt2005 <resart2005@gmail.com>
2024-11-20 23:00:18 +0400

    f

--- LR5/src/main/java/ru/ssau/tk/ArtKsenInc/OOP_JAVA/Main.java
+++ LR5/src/main/java/ru/ssau/tk/ArtKsenInc/OOP_JAVA/Main.java
@@ -1,9 +1,13 @@
 package ru.ssau.tk.ArtKsenInc.OOP_JAVA;
 
 import ru.ssau.tk.ArtKsenInc.OOP_JAVA.functions.*;
+import ru.ssau.tk.ArtKsenInc.OOP_JAVA.ui.MainWindow;
+
+import javax.swing.*;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.print("Hello");
+        // Создание и отображение главного окна
+        SwingUtilities.invokeLater(MainWindow::new);
     }
 }
