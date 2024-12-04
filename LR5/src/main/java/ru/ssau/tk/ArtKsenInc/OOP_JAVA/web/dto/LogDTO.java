9740e38b77307cc36a27c761b82f285b2c86785e
ResArt2005 <resart2005@gmail.com>
2024-12-04 21:15:28 +0400

    Добавление DTO

--- LR5/src/main/java/ru/ssau/tk/ArtKsenInc/OOP_JAVA/web/dto/LogDTO.java
+++ LR5/src/main/java/ru/ssau/tk/ArtKsenInc/OOP_JAVA/web/dto/LogDTO.java
@@ -1,4 +1,15 @@
 package ru.ssau.tk.ArtKsenInc.OOP_JAVA.web.dto;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+import java.sql.Timestamp;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class LogDTO {
+    private int id;
+    private String message;
+    private Timestamp timestamp;
 }
--- LR5/src/main/java/ru/ssau/tk/ArtKsenInc/OOP_JAVA/web/dto/MathFuncDTO.java
+++ LR5/src/main/java/ru/ssau/tk/ArtKsenInc/OOP_JAVA/web/dto/MathFuncDTO.java
@@ -1,4 +1,15 @@
 package ru.ssau.tk.ArtKsenInc.OOP_JAVA.web.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class MathFuncDTO {
+    private int id;
+    private double x;
+    private double y;
+    private long hash;
 }
