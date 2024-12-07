0b469e28175e78676600e86aaf5528eebe7796aa
Kseteny <1kseteni1@gmail.com>
2024-12-07 20:19:54 +0400

    MathFuncDTOTest, MathFuncReposTest и MathFuncTest

---
+++ LR5/src/test/java/SpringTests/MathFuncReposTest.java
@@ -0,0 +1,56 @@
+package SpringTests;
+
+import ru.ssau.tk.ArtKsenInc.OOP_JAVA.jpa.entities.MathFunc;
+import ru.ssau.tk.ArtKsenInc.OOP_JAVA.jpa.repository.MathFuncRepos;
+import ru.ssau.tk.ArtKsenInc.OOP_JAVA.functions.MathFunction;
+import ru.ssau.tk.ArtKsenInc.OOP_JAVA.Main;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest(classes = Main.class)
+
+public class MathFuncReposTest {
+
+    @Autowired
+    private MathFuncRepos mathFuncRepos;
+
+    @Test
+    public void testSaveAndFindMathFunc() {
+        String name = "testFunction";
+        MathFunction function = new MathFunction() {
+            @Override
+            public double apply(double x) {
+                return x * x;
+            }
+        };
+        MathFunc mathFunc = new MathFunc(name, function);
+        mathFuncRepos.save(mathFunc);
+
+        Optional<MathFunc> foundMathFunc = mathFuncRepos.findById(mathFunc.getId());
+        assertTrue(foundMathFunc.isPresent());
+        assertEquals(name, foundMathFunc.get().getName());
+        assertEquals(function.getClass().getSimpleName(), foundMathFunc.get().getFunction().getClass().getSimpleName());
+    }
+
+    @Test
+    public void testDeleteMathFunc() {
+        String name = "testFunction";
+        MathFunction function = new MathFunction() {
+            @Override
+            public double apply(double x) {
+                return x * x;
+            }
+        };
+        MathFunc mathFunc = new MathFunc(name, function);
+        mathFuncRepos.save(mathFunc);
+
+        mathFuncRepos.deleteById(mathFunc.getId());
+        Optional<MathFunc> deletedMathFunc = mathFuncRepos.findById(mathFunc.getId());
+        assertFalse(deletedMathFunc.isPresent());
+    }
+}
---
+++ LR5/src/test/java/SpringTests/MathFuncTest.java
@@ -0,0 +1,80 @@
+package SpringTests;
+
+import ru.ssau.tk.ArtKsenInc.OOP_JAVA.jpa.entities.MathFunc;
+import ru.ssau.tk.ArtKsenInc.OOP_JAVA.functions.CompositeFunction;
+import ru.ssau.tk.ArtKsenInc.OOP_JAVA.functions.MathFunction;
+import ru.ssau.tk.ArtKsenInc.OOP_JAVA.Main;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest(classes = Main.class)
+
+public class MathFuncTest {
+
+    @Test
+    public void testNoArgsConstructor() {
+        MathFunc mathFunc = new MathFunc();
+        assertNotNull(mathFunc);
+    }
+
+    @Test
+    public void testAllArgsConstructor() {
+        String name = "testFunction";
+        MathFunction function = new MathFunction() {
+            @Override
+            public double apply(double x) {
+                return x * x;
+            }
+        };
+        MathFunc mathFunc = new MathFunc(name, function);
+
+        assertEquals(name, mathFunc.getName());
+        assertEquals(function.getClass().getSimpleName(), mathFunc.getFunction().getClass().getSimpleName());
+    }
+
+    @Test
+    public void testSettersAndGetters() {
+        MathFunc mathFunc = new MathFunc();
+        String name = "testFunction";
+        MathFunction function = new MathFunction() {
+            @Override
+            public double apply(double x) {
+                return x * x;
+            }
+        };
+
+        mathFunc.setName(name);
+        mathFunc.setFunction(function);
+
+        assertEquals(name, mathFunc.getName());
+        assertEquals(function.getClass().getSimpleName(), mathFunc.getFunction().getClass().getSimpleName());
+    }
+
+    @Test
+    public void testCompositeFunction() {
+        MathFunction firstFunction = new MathFunction() {
+            @Override
+            public double apply(double x) {
+                return x + 1;
+            }
+        };
+        MathFunction secondFunction = new MathFunction() {
+            @Override
+            public double apply(double x) {
+                return x * x;
+            }
+        };
+        CompositeFunction compositeFunction = new CompositeFunction(firstFunction, secondFunction);
+
+        MathFunc mathFunc = new MathFunc("compositeFunction", compositeFunction);
+
+        assertEquals("compositeFunction", mathFunc.getName());
+        assertTrue(mathFunc.getFunction() instanceof CompositeFunction);
+
+        CompositeFunction parsedFunction = (CompositeFunction) mathFunc.getFunction();
+        assertEquals(firstFunction.getClass().getSimpleName(), parsedFunction.getFirstFunction().getClass().getSimpleName());
+        assertEquals(secondFunction.getClass().getSimpleName(), parsedFunction.getSecondFunction().getClass().getSimpleName());
+    }
+}
