2499ae93ba62d5c0a31475e56eb7609a70025c0b
ResArt2005 <resart2005@gmail.com>
2024-12-26 02:25:47 +0400

    Контроллер для пользователей

--- LR7 (thymleaf)/src/main/java/ru/ssau/tk/ArtKsenInc/OOP_JAVA/ui/WorkWithDBController.java
+++ LR7 (thymleaf)/src/main/java/ru/ssau/tk/ArtKsenInc/OOP_JAVA/ui/WorkWithDBController.java
@@ -1,36 +1,117 @@
 package ru.ssau.tk.ArtKsenInc.OOP_JAVA.ui;
 
+import jakarta.servlet.http.HttpSession;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.SessionAttributes;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+import ru.ssau.tk.ArtKsenInc.OOP_JAVA.jpa.dto.UserDTO;
+import ru.ssau.tk.ArtKsenInc.OOP_JAVA.jpa.entities.User;
 import ru.ssau.tk.ArtKsenInc.OOP_JAVA.ui.special_classes.dbTools;
 
+import java.util.HashMap;
+import java.util.Map;
+
 @Controller
-@SessionAttributes("fabricType")
+@SessionAttributes({"fabricType", "userDTO"})
 public class WorkWithDBController {
+    //Инициализация страницы для работы с бд
     @GetMapping("/main/workWithDb")
-    public String main() {
+    public String get(Model model, HttpSession session) {
+        UserDTO user = (UserDTO) session.getAttribute("userDTO");
+        if(user == null || !user.getToken().equals("admin")){
+            return "redirect:/";
+        }
+        model.addAttribute("users", "");
         return "workWithDb";
     }
-
-    @PostMapping("/main/workWithDbLogs")
-    public void workWithDbLogs() {
-
+    //Методы для работы с пользователями
+    @PostMapping("/main/workWithDbUsersINNT")
+    @ResponseBody
+    public String workWithDbUsers() {
+        Map<String, User> users = dbTools.getAllUsers();
+        if(users.size() == 1){
+            return "<div class='userBlock'>Пользователей кроме админа не обнаружено</div>";
+        }
+        return userTable(users);
+    }
+    @PostMapping("/main/UserCreate")
+    @ResponseBody
+    public String UserCreate(@RequestParam("login") String login,
+                           @RequestParam("password") String password){
+        User newUser = new User(login, password);
+        dbTools.createUser(newUser);
+        Map<String, User> users = dbTools.getAllUsers();
+        dbTools.createLog(login + " добавился в систему");
+        return userTable(users);
+    }
+    @PostMapping("/main/UserChange")
+    @ResponseBody
+    public String UserChange(@RequestParam("token") String token,
+                           @RequestParam("login") String login,
+                           @RequestParam("password") String password){
+        if (token.equals("admin")){
+            Map<String, User> users = dbTools.getAllUsers();
+            return userTable(users);
+        }
+        dbTools.updateUserByToken(token, login, password);
+        Map<String, User> users = dbTools.getAllUsers();
+        return userTable(users);
+    }
+    @PostMapping("/main/UserDelete")
+    @ResponseBody
+    public String UserDelete(@RequestParam("token") String token, Model model){
+        if (token.equals("admin")){
+            Map<String, User> users = dbTools.getAllUsers();
+            return userTable(users);
+        }
+        dbTools.deleteUserByToken(token);
+        Map<String, User> users = dbTools.getAllUsers();
+        return userTable(users);
     }
 
+    private String userTable(Map<String, User> users){
+        StringBuilder sb = new StringBuilder();
+        int id = 0;
+        sb.append("<div class='userBlock'>");
+        for (String token: users.keySet()){
+            sb.append("<div><span class='userToken' id='userToken").append(id).append("'>")
+                    .append(token).append("</span> <span class='userLogin'>")
+                    .append(users.get(token).getLogin())
+                    .append("</span><button class='userChangeBtn' data-token-id='userToken")
+                    .append(id).append("'>Изменить</button> <button class='userDeleteBtn' data-token-id='userToken")
+                    .append(id).append("'>Удалить</button></div>");
+            ++id;
+        }
+        sb.append("</div>");
+        return sb.toString();
+    }
+    //Методы для работы с математическими функциями
     @PostMapping("/main/workWithDbMathFunc")
-    public void workWithDbMathFunc() {
+    @ResponseBody
+    public void workWithDbMathFunc(HttpSession session) {
 
     }
-
+    //Методы для работы с табулированными функциями
     @PostMapping("/main/workWithDbTBFunc")
-    public void workWithDbTBFunc() {
+    @ResponseBody
+    public void workWithDbTBFunc(HttpSession session) {
 
     }
 
-    @PostMapping("/main/workWithDbUsers")
-    public void workWithDbUsers() {
+    //Методы для работы с логами
+    @PostMapping("/main/workWithDbLogs")
+    @ResponseBody
+    public void workWithDbLogs(HttpSession session) {
 
+    }
+
+    //Стирание всех данных из базы
+    @PostMapping("/main/workWithDbEraseAll")
+    @ResponseBody
+    public void workWithDbEraseAll() {
+        dbTools.deleteAllUsers();
+        dbTools.deleteAllMathFunctions();
+        dbTools.deleteAllTBFunctions();
+        dbTools.deleteAllLogs();
     }
 }
--- LR7 (thymleaf)/src/main/resources/static/js/workWithBDAjax.js
+++ LR7 (thymleaf)/src/main/resources/static/js/workWithBDAjax.js
@@ -1,3 +1,24 @@
+// Копирование токена в буфер обмена
+const tokenText = document.getElementById('tokenText');
+const copySuccessMessage = document.getElementById('copySuccessMessage');
+if (tokenText) {
+    tokenText.addEventListener('click', () => {
+        const token = tokenText.textContent; // Получаем текст токена
+
+        // Используем API для записи текста в буфер обмена
+        navigator.clipboard.writeText(token).then(() => {
+            // Показываем сообщение об успешном копировании
+            copySuccessMessage.style.display = 'inline';
+
+            // Скрываем сообщение через 2 секунды
+            setTimeout(() => {
+                copySuccessMessage.style.display = 'none';
+            }, 2000);
+        }).catch(err => {
+            console.error('Ошибка при копировании текста:', err);
+        });
+    });
+}
 //Открытие окна
 function showModal(modalId){document.getElementById(modalId).style.display = "block"; }
 // Закрытие модального окна
@@ -41,3 +62,135 @@ document.querySelectorAll('art_byArr_pointsCount').forEach(input =>{
 });
 //----------------------------------------------------------------------\\
 //Здесь Аякс запросы
+//Очистить базу
+document.getElementById("eraseBtn").addEventListener('click', function(){
+    fetch("/main/workWithDbEraseAll",{
+        method: "POST"
+    }).then(response=>{
+        if(response.ok){
+            Message("success", "База чиста!");
+            location.reload();
+        }
+        else{
+            Message("error", "Что-то пошло не так на стороне сервера!");
+        }
+    }).catch(error => {
+        Message("error", "Ошибка сети: " + error.message);
+    });
+});
+//Работа с пользователями
+//Кнопка, открывающая окно настроек пользователей
+document.getElementById("userButton").addEventListener("click", function(){
+    fetch("/main/workWithDbUsersINNT", {
+        method: "POST"
+    }).then(response => {
+        if (response.ok) {
+            return response.text();
+        } else {
+            Message("error", "Что-то пошло не так на стороне сервера!");
+        }
+    }).then(data => {
+        const userContainer = document.getElementById("userContainer");
+        userContainer.innerHTML = data;
+        addDeleteEventListeners();
+        addChangeEventListeners();
+    }).catch(error => {
+        Message("error", "Ошибка сети: " + error.message);
+    });
+});
+//Слушатели событий для сгенерированных кнопок из сервера Удалить и Изменить
+function addDeleteEventListeners() {
+    document.querySelectorAll(".userDeleteBtn").forEach(button => {
+        button.addEventListener('click', function(){
+            const token = document.getElementById(this.getAttribute("data-token-id")).innerText;
+            fetch("/main/UserDelete", {
+                method: "POST",
+                headers: {
+                    'Content-Type': 'application/x-www-form-urlencoded'
+                },
+                body: 'token=' + encodeURIComponent(token)
+            }).then(response => {
+                if (response.ok) {
+                    return response.text();
+                } else {
+                    Message("error", "Что-то пошло не так на стороне сервера!");
+                }
+            }).then(data => {
+                const userContainer = document.getElementById("userContainer");
+                userContainer.innerHTML = data;
+                addDeleteEventListeners(); // Перепривязываем обработчики событий для новых элементов
+            }).catch(error => {
+                Message("error", "Ошибка сети: " + error.message);
+            });
+        });
+    });
+}
+function addChangeEventListeners() {
+    document.querySelectorAll(".userChangeBtn").forEach(button => {
+        button.addEventListener('click', function(){
+            const token = document.getElementById(this.getAttribute("data-token-id")).innerText;
+            showModal("userChanging");
+            document.getElementById("changeUser").addEventListener("click", function(){
+            const login = document.getElementById("login_to_change").value;
+            const password = document.getElementById("password_to_change").value;
+            if(login == '' || password ==''){
+                Message("error", "Введите логин и пароль пользователя!");
+                return;
+            }
+            fetch("/main/UserChange", {
+                method: "POST",
+                headers: {
+                    'Content-Type': 'application/x-www-form-urlencoded'
+                },
+                body: 'token=' + encodeURIComponent(token) +"&"+'login=' + encodeURIComponent(login)+"&"+"password="+encodeURIComponent(password)
+            }).then(response => {
+                if (response.ok) {
+                    return response.text();
+                } else {
+                    Message("error", "Что-то пошло не так на стороне сервера!");
+                }
+            }).then(data => {
+               hideModal("userChanging");
+               const userContainer = document.getElementById("userContainer");
+               userContainer.innerHTML = data;
+               addChangeEventListeners(); // Перепривязываем обработчики событий для новых элементов
+            }).catch(error => {
+                Message("error", "Ошибка сети: " + error.message);
+            });
+        });
+    });
+    });
+}
+//Регистрация новых пользователей
+document.getElementById("registerUser").addEventListener("click", function(){
+    login = document.getElementById("login").value;
+    password = document.getElementById("password").value;
+    if(login == '' || password ==''){
+        Message("error", "Введите логин и пароль пользователя!");
+        return;
+    }
+    fetch("/main/UserCreate",{
+        method: "POST",
+        headers: {
+            'Content-Type': 'application/x-www-form-urlencoded'
+        },
+        body: 'login=' + encodeURIComponent(login)+"&"+"password="+encodeURIComponent(password)
+    }).then(response=>{
+        if(response.ok){
+            Message("success", "Пользователь зарегистрирован!");
+            return response.text()
+        }
+        else{
+            Message("error", "Ошибка регистрации!");
+        }
+    }).then(data=>{
+        hideModal("userCreation")
+        document.getElementById("login").value = '';
+        document.getElementById("password").value = '';
+        userContainer = document.getElementById("userContainer");
+        userContainer.innerHTML = data;
+    }).catch(error => {
+        Message("error", "Ошибка сети: " + error.message);
+    });
+});
+
